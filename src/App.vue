a8ab171e8ef02c387deb8dc59f26ecf519487cd9
Casie Lynch <casie@primeacademy.io>
2017-08-02 18:10:58 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <img src="./assets/logo.png">
+     </div>
+    <router-view v-bind:guests="Guests" v-bind:companies="Companies" v-bind:templates="Templates"></router-view>
+  </div>
+</template>
+
+<script>
+import Guests from './jsonFiles/Guests.json';
+import Companies from './jsonFiles/Companies.json';
+import Templates from './jsonFiles/Templates.json';
+
+export default {
+  name: 'app',
+  components: {
+    Guests,
+    Companies,
+    Templates
+  },
+  data(){
+    return{
+        Guests: Guests,
+        Companies: Companies,
+        Templates: Templates,
+    }
+  },
+  beforeMount() {
+        console.log(Companies);
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+
+.header {
+  height: 100px;
+  background-color: #1d5dab;
+  padding: 20px;
+}
+
+img{
+  height: 90px;
+}
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="text-compose">
+        <h1>Hello!</h1>
+        <div class="chooseTemplate">
+            <h2>What template would you like to use?</h2>
+            <div v-for="template in templates">
+                <input type="radio" :id="template.name" :value="template.name" v-model="thisShit" v-on:click="fire">
+                <label :for="template.name">{{template.message}}</label>
+            </div>
+            <span>Picked: {{ thisShit }}</span>
+        </div>
+
+
+
+
+
+
+
+        <div class="chooseGuest">
+            <h2>Choose a guest</h2>
+            <ul>
+                <li v-for="guest in guests">
+                    <button type="radio"></button>
+                    {{guest.firstName}}
+                </li>
+            </ul>
+        </div>
+        <div class="chooseHotel">
+            <h2>Choose your hotel</h2>
+            <ul>
+                <li v-for="company in companies">
+                    <button type="radio"></button>
+                    {{company.company}}
+                </li>
+            </ul>
+        </div>
+        <div class="finalMessage">
+            <h2>Here is your final message</h2>
+            <div class="displayMsg"></div>
+            <button class="sendMsg">Send</button>
+            <button class="editMsg">Edit</button>
+            <button class="cancelMsg">Cancel</button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'text-compose',
+  props: ['guests', 'companies', 'templates'],
+  data() {
+      return{
+          thisShit: '',
+      }
+  },
+  methods: {
+      fire(){
+          console.log(this, this.name);
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
